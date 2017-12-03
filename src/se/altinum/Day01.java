b87511918de893144cdc14d346604eedcc776dcf
Magnus Altin <magnus.altin@gmail.com>
2017-12-03 15:24:15 +0100

    Solution to Day 1

---
+++ src/se/altinum/Day01.java
@@ -0,0 +1,23 @@
+package se.altinum;
+
+class Day01 {
+    static int captcha(String digits) {
+
+        char[] intArray = digits.toCharArray();
+
+        int sum = 0;
+        for (int i = 0; i < intArray.length; i++) {
+            Integer x = Integer.parseInt(String.valueOf(intArray[i]));
+
+            if (i < intArray.length -1) {
+                if (intArray[i] == intArray[i + 1]) {
+                    sum += x;
+                }
+            } else if (intArray[i] == intArray[0]) {
+                sum += x;
+            }
+        }
+        System.out.println(sum);
+        return sum;
+    }
+}
