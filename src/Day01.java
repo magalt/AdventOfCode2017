679f8f3376879d377593437e059bad49d0c9448a
Magnus Altin <magnus.altin@gmail.com>
2017-12-06 22:29:38 +0100

    Solution to Day 1 - part 2

--- src/Day01.java
+++ src/Day01.java
@@ -18,4 +18,27 @@ class Day01 {
         System.out.println(sum);
         return sum;
     }
+
+    static int captcha2(String digits) {
+      char[] intArray = digits.toCharArray();
+
+      int size = digits.length();
+      int ahead = digits.length() / 2;
+
+      int sum = 0;
+
+      for (int i = 0; i < intArray.length; i++) {
+        Integer x = Integer.parseInt(String.valueOf(intArray[i]));
+        Integer y = Integer.parseInt(String.valueOf(intArray[(i+ahead)%size]));
+
+        if (x == y) {
+          sum += x;
+        }
+      }
+
+      System.out.println(sum);
+      return sum;
+    }
+
+
 }
